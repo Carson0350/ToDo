e762aa0b2bd22793e1d33fcc265d8869ac3e441f
Carson0350 <carson.workflow@gmail.com>
2020-09-30 09:48:38 -0600

    server connected, database connected

---
+++ database/index.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'localhot',
+  user: 'postgres',
+  port: 3000,
+  password: 'password',
+  database: 'postgres',
+  max: 20,
+});
+
+pool.connect()
+  .then(() => console.log('connected to a database'))
+  .catch((err) => console.log(err));
+
+module.exports = pool;
