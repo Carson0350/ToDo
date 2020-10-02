87f2c652c938e51382de576de6123aeedbe50281
Carson0350 <carson.workflow@gmail.com>
2020-10-02 13:44:54 -0600

    auth styles added

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,76 +1,72 @@
-import React from 'react';
-// import child components.
-import axios from 'axios';
-import Navigation from './Navigation.jsx';
-// import axios from 'axios';
+import React, { useState } from 'react';
+import Auth from './Auth.jsx';
 
 const api = {
   key: '49a9c03d5547e8fc48a1f81cff3bf6d0',
-  base: 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=',
+  base: 'https://api.openweathermap.org/data/2.5/',
 };
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      apiData: [],
-    };
-  }
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
-  componentDidMount() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=49a9c03d5547e8fc48a1f81cff3bf6d0')
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response.main);
-        console.log(response.name);
-        console.log(response.weather);
-        console.log(response.wind);
-        console.log(response.sys);
-        this.setState({
-          apiData: response,
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
         });
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }
+    }
+  };
 
-  render() {
-    const dateBuilder = (d) => {
-      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const dateBuilder = (d) => {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-      return `${day} ${date} ${month} ${year}`
-    };
+    const day = days[d.getDay()];
+    const date = d.getDate();
+    const month = months[d.getMonth()];
+    const year = d.getFullYear();
 
-    return (
-      <div className='app warm'>
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  return (
+    <div>
+      <Auth />
+      <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
         <main>
           <div className='search-box'>
             <input
               type='text'
               className='search-bar'
               placeholder='search...'
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
             />
           </div>
-          <div className='location-box'>
-            <div className='location'>Denver Colorado</div>
-            <div className='date'>{dateBuilder(new Date())}</div>
-          </div>
-          <div className='weather-box'>
-            <div className='temp'>
-              75F
+          {(typeof weather.main != "undefined") ? (
+          <div>
+              <div className='location-box'>
+              <div className='location'>{weather.name}, {weather.sys.country}</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
             </div>
-            <div className='weather'>Beautiful</div>
+            <div className='weather-box'>
+              <div className='temp'>
+                {Math.round(weather.main.temp)}°c
+              </div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
           </div>
+          ) : ('')}
         </main>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+
+const Auth = (props) => (
+  <div>
+    <div className="auth-container">
+      <div className="auth-box">
+        <input
+          className="userName-bar"
+          type="text"
+          placeholder="userName"
+        />
+        <input
+          className="password-bar"
+          type="text"
+          placeholder="password"
+        />
+        <button
+          type="button"
+          className="btn"
+        >
+          Login!
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default Auth;
