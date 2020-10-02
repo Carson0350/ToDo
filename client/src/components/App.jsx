f4ad04347a3889a15c0b22f61783ec1ad28ff254
Carson0350 <carson.workflow@gmail.com>
2020-10-02 10:08:10 -0600

    even more style

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -36,7 +36,6 @@ class App extends React.Component {
   }
 
   render() {
-
     const dateBuilder = (d) => {
       let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
       let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
